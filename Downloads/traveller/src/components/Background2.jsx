cb7b684263cda3a23fe8eab959181a2c5a6ce31c
piriinka <piriina.nipeijun@gmail.com>
2023-12-05 03:58:06 +0800

    first commit

---
+++ Downloads/traveller/src/components/Background2.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+import {useSpring, animated} from "@react-spring/web";
+import bg1 from '../assets/bg1.jpg'
+
+
+export const Background = () => {
+    const props = useSpring({
+        from: { opacity: 0 },
+        to: { opacity: 1 },
+    })
+    return (
+        <animated.div style={{
+            ...props,
+            backgroundImage: `url(${bg1})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            width: '100vw',
+            height: '100vh',
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            zIndex: -1
+        }}/>
+    );
+};
